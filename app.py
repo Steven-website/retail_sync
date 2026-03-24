3c98728528c8c8f1303920658259e9ea31b8e51c
Steven-website <135800079+Steven-website@users.noreply.github.com>
2026-03-24 14:31:44 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 from auth import init_session, login_view, sidebar_usuario
 from config import ROL_MASTER, ROL_ADC, ROL_VISUALIZADOR
-from roles.master_view      import master_view
-from roles.adc_view         import adc_view
+from roles.master_view       import master_view
+from roles.adc_view          import adc_view
 from roles.visualizador_view import visualizador_view
 
 st.set_page_config(page_title="Retail Sync", layout="wide")
@@ -17,8 +17,8 @@ st.divider()
 sidebar_usuario()
 
 ROUTER = {
-    ROL_MASTER:      master_view,
-    ROL_ADC:         adc_view,
+    ROL_MASTER:       master_view,
+    ROL_ADC:          adc_view,
     ROL_VISUALIZADOR: visualizador_view,
 }
 
